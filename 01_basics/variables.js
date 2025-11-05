a440f9cd463b7c57f8bebc44d8bd28633f850e98
Shradha-Gaba <shradhagaba23@gmail.com>
2025-11-05 06:20:25 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = "144553"
+let accountEmail="shra@123"
+var accountPassword="123"
+accountCity="Raipur"
+
+//accountId=2 // not allowed
+accountEmail="gha@123"
+accountCity = "saharnpur"
+accountPassword = "1123"
+let accountState
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+//two methods to declare variable which is let and var but we dont use var
+/* prefer not to use var because of issue in block scope and functional scope*/
+
