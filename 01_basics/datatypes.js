4d468a6cdce0962b528d4f64de67b9dd44217fc9
Shradha-Gaba <shradhagaba23@gmail.com>
2025-11-06 22:04:15 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treats all JS code as newer version
+
+//alert(3 + 5) //we are using node js not browser
+
+console.log("hi ia am shradha")
+console.log(4+3)
+
+let name="shradha"
+let age=12
+let IsLoggedIn= false
+let state;
+  //PRIMITIVE DATATTYPE
+// range of number = 2 to the power 53
+// BigInt
+// string = ""
+// boolean = true/false
+// null=standalone value
+// undefined - means value is not defined
+// symbol- to find uniqueness we use symbol tool
+
+
+// OBJECT
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof  undefined)
