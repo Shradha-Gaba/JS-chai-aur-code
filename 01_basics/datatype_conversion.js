eaf9f4cc771b90ec04445aa53452b1cbe795baa6
Shradha-Gaba <shradhagaba23@gmail.com>
2025-11-07 16:58:37 +0000

    datatype

--- 01_basics/datatype_conversion.js
+++ 01_basics/datatype_conversion.js
@@ -13,3 +13,17 @@ console.log(valueInNumber);
 // true=>1
 //false=>0
 // "shradha"
+
+let isLoggedIn = 1 
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+
+//1=>True;  0 => False
+// "" => false
+//"hitesh" => true
+
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber) ;
+console.log(typeof stringNumber);
