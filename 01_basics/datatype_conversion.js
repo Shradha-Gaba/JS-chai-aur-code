e86dc3527b9c26022520d2679b4c1250908d4212
Shradha-Gaba <shradhagaba23@gmail.com>
2025-11-07 11:22:56 +0000

    Tconversion

---
+++ 01_basics/datatype_conversion.js
@@ -0,0 +1,15 @@
+let score = 33 // also try "33","33abc",null,undefined,true,"shradha"
+console.log(typeof score);
+console.log(typeof(score)); //second way of writting
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+// "33abc"=>NaN
+// null
+// undefined
+// true=>1
+//false=>0
+// "shradha"
